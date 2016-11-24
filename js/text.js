d7e97652a8cbbf496ad197bbb1b9dc743ff253c3
Gianluca Mauro <Gianluca Mauro>
2016-11-24 18:27:01 +0100

    First commit website

---
+++ js/text.js
@@ -0,0 +1,114 @@
+// REMEMBER: the video url format must be like "player.vimeo.com/-----"
+
+var main_subtitle_es = "Dirección Artistica";
+var main_subtitle_en = "Production Designer";
+var title_about_section_es = "Hola, que tal?";
+var title_about_section_en = "Hello, how are you?";
+var description_about_section_es = 'Creo que la dirección artística debe ser honesta y limpia, pasar casi inadvertida. Debe emocionar y acompañar al resto de elementos que llenan la pantalla. Conseguir un equilibrio prestando atención hasta al detalle más mínimo y nunca debe perder su intención. </br><br/> \
+	Me llamo Alexandra y soy directora artística en cine. Vivo en Barcelona, mi ciudad favorita, aunque por mi condición de nómada no descarto volar hacia otro destino en busca de un proyecto desafiante, salvaje. <br/><br/> \
+	Fetichista de los objetos, de la simplicidad y del caos, he trabajado en un laboratorio de color, he hecho de corresponsal en Bèlgica, he sido Community Manager y he realizado fashion films, videos y fotografías para diferentes empresas. <br/><br/> \
+	“All creative work begines by doing something with the hands. Creation is simly a problem and design is the way out”. </br>Charles James⁠⁠⁠⁠';
+var description_about_section_en = "I'm the bellaest";
+
+
+// ----------- SPANISH -------------
+var works_es = [
+{
+	title: "MOTXILLES",
+	subtitle: "Asistente de arte",
+	image: "motxilles.jpg",
+	info_title: "JULIO 2016",
+	info: "Exteriorizar los sentimientos y emociones es el objetivo de esta obra teatral, hablar de las mochilas que todos cargamos a las espaldas. ¿Qué es y qué implica la amistad? El argumento de “Motxilles” gira entorno a esta pregunta. La proximidad física que se consigue en el Microteatre Barcelona entre el público y las actrices hará de esta obra una experiencia íntima y cercana, no apta para personas que con miedo a meter mano a sus mochilas emocionales para así vaciarlas, mostrarlas, compartirlas, exteriorizarlas. La escenografía de la obra necesitaba reflejar feminidad y sensualidad. Para ello, recreamos un espacio neutro donde el protagonismo se lo dábamos al techo con un conjunto de telas y gasas satinadas, livianas y semitransparentes de colores pastel, con una caída a modo de jaima cuyo resultado se nos antoja femenino y puro.",
+	video: "https://player.vimeo.com/video/176533050",
+},
+{
+	title: "BIENVENIDO",
+	subtitle: "Dirección de arte",
+	image: "BIENVENIDO.png",
+	info_title: "ESCAC / Abril 2016",
+	info: "Mario, un hombre de unos 35 años, vuelve a su antigua y espaciosa casa el día de navidad después de años sin ver a su familia. Su madre, padre y hermano menor le esperan extrañamente amables y con la cena preparada.",
+	video: "https://player.vimeo.com/video/184845407",
+},
+{
+	title: "AMARA",
+	subtitle: "Atrezzista",
+	image: "AMARA.png",
+	info_title: "ESCAC / Junio 2016",
+	info: "Marcos observa el mundo desde su ventana mientras cuida a su madre, una mujer con un trastorno de personalidad por dependencia provocado por la pérdida de su marido. Conocer a Eva, una chica que vive en su mismo edificio, hará que se enfrente a la decisión más difícil de su vida: seguir cediendo al chantaje de su madre o empezar a vivir su vida.",
+	video: "https://player.vimeo.com/video/184351115",
+},
+{
+	title : "MICROAMOR",
+	subtitle : "Dirección artística",
+	image : "MICROAMOR.jpg",
+	info_title : "ECIB / Mayo 2016",
+	info : "Carlos trabaja en una cadena de despiece de carne. Su rutina, trabajo y después a casa. Se prepara la cena. Ve la televisión. Se va a su habitación, se masturba y se echa a dormir. Vuelve al trabajo. Vive una vida tranquila, pero carente de emociones, rutinaria y plana, en una sociedad en la que no termina de encajar. Sus miedos e inseguridades le impiden relacionarse con otras personas. Las nuevas tecnologías le dan el empujón que le hace falta para buscar el amor que no tiene en su vida, pero pronto se da cuenta de que estos nuevos medios tienen su parte negativa, y la otra opción que le queda llega demasiado tarde.",
+	video: "https://player.vimeo.com/video/176533050", //VIDEO WRONG
+},
+{
+	title: "H V M A N X T E R R A",
+	subtitle: "Dirección artística",
+	image: "HVMANXTERRA.png",
+	info_title: "Mayo 2014",
+	info: "H V M A N X T E R R A trata la relación histórica del ser humano y la Tierra a través del sexo. Dividido en tres etapas, H V M A N X T E R R A muestra el desarrollo de las fuerzas productivas. Las etapas son: etapa de descubrimiento (TERRA es visitada por HVMAN que, como elemento novedoso, empieza a relacionarse con ella); etapa de explotación de las fuerzas productivas (dicha explotación se ve reflejada en la piedra, la hoz, la industria y la extracción de petróleo) y etapa de culminación o agotamiento. La culminación introduce una actitud autoritaria de HVMAN sobre TERRA.",
+	video: "https://player.vimeo.com/video/96937212",
+},
+//add above
+]
+
+// ----------- ENGLISH -------------
+
+var works_en = [
+{
+	title: "MOTXILLES",
+	subtitle: "Artistic Director",
+	image: "motxilles.jpg",
+	info_title: "JULIO 2016",
+	info: "Exteriorizar los sentimientos y emociones es el objetivo de esta obra teatral, hablar de las mochilas que todos cargamos a las espaldas. ¿Qué es y qué implica la amistad? El argumento de “Motxilles” gira entorno a esta pregunta. La proximidad física que se consigue en el Microteatre Barcelona entre el público y las actrices hará de esta obra una experiencia íntima y cercana, no apta para personas que con miedo a meter mano a sus mochilas emocionales para así vaciarlas, mostrarlas, compartirlas, exteriorizarlas. La escenografía de la obra necesitaba reflejar feminidad y sensualidad. Para ello, recreamos un espacio neutro donde el protagonismo se lo dábamos al techo con un conjunto de telas y gasas satinadas, livianas y semitransparentes de colores pastel, con una caída a modo de jaima cuyo resultado se nos antoja femenino y puro.",
+	video: "https://player.vimeo.com/video/176533050",
+},
+{
+	title: "BIENVENIDO",
+	subtitle: "Dirección de arte",
+	image: "BIENVENIDO.png",
+	info_title: "ESCAC / Abril 2016",
+	info: "Mario, un hombre de unos 35 años, vuelve a su antigua y espaciosa casa el día de navidad después de años sin ver a su familia. Su madre, padre y hermano menor le esperan extrañamente amables y con la cena preparada.",
+	video: "https://player.vimeo.com/video/184845407",
+},
+{
+	title: "AMARA",
+	subtitle: "Atrezzista",
+	image: "AMARA.png",
+	info_title: "ESCAC / Junio 2016",
+	info: "Marcos observa el mundo desde su ventana mientras cuida a su madre, una mujer con un trastorno de personalidad por dependencia provocado por la pérdida de su marido. Conocer a Eva, una chica que vive en su mismo edificio, hará que se enfrente a la decisión más difícil de su vida: seguir cediendo al chantaje de su madre o empezar a vivir su vida.",
+	video: "https://player.vimeo.com/video/184351115",
+},
+{
+	title : "MICROAMOR",
+	subtitle : "Dirección artística",
+	image : "MICROAMOR.jpg",
+	info_title : "ECIB / Mayo 2016",
+	info : "Carlos trabaja en una cadena de despiece de carne. Su rutina, trabajo y después a casa. Se prepara la cena. Ve la televisión. Se va a su habitación, se masturba y se echa a dormir. Vuelve al trabajo. Vive una vida tranquila, pero carente de emociones, rutinaria y plana, en una sociedad en la que no termina de encajar. Sus miedos e inseguridades le impiden relacionarse con otras personas. Las nuevas tecnologías le dan el empujón que le hace falta para buscar el amor que no tiene en su vida, pero pronto se da cuenta de que estos nuevos medios tienen su parte negativa, y la otra opción que le queda llega demasiado tarde.",
+	video: "https://player.vimeo.com/video/",
+},
+{
+	title: "H V M A N X T E R R A",
+	subtitle: "Dirección artística",
+	image: "HVMANXTERRA.png",
+	info_title: "Mayo 2014",
+	info: "H V M A N X T E R R A trata la relación histórica del ser humano y la Tierra a través del sexo. Dividido en tres etapas, H V M A N X T E R R A muestra el desarrollo de las fuerzas productivas. Las etapas son: etapa de descubrimiento (TERRA es visitada por HVMAN que, como elemento novedoso, empieza a relacionarse con ella); etapa de explotación de las fuerzas productivas (dicha explotación se ve reflejada en la piedra, la hoz, la industria y la extracción de petróleo) y etapa de culminación o agotamiento. La culminación introduce una actitud autoritaria de HVMAN sobre TERRA.",
+	video: "https://player.vimeo.com/video/96937212",
+},
+]
+
+
+
+
+// {
+// 	title: "",
+// 	subtitle: "",
+// 	image: "",
+// 	info_title: "",
+// 	info: "",
+// 	video_code: "",
+// },
